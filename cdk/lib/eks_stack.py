fc27fb12e4413dc0879639d50c758be9d7b642dc
Junsu An <anjjunsu@gmail.com>
2024-10-29 20:11:11 -0700

    [G5T-11] Define DAST deployment in EKS

--- cdk/lib/eks_stack.py
+++ cdk/lib/eks_stack.py
@@ -1,3 +1,5 @@
+import yaml
+
 from aws_cdk import (
     Stack,
     aws_ec2 as ec2,
@@ -5,13 +7,31 @@ from aws_cdk import (
 )
 from constructs import Construct
 
+
 class EksStack(Stack):
+    dast_deployment_path = "eks_manifests/dast_deployment.yaml"
+    dast_service_path = "eks_manifests/dast_deployment.yaml"
+
     def __init__(self, scope: Construct, id: str, vpc: ec2.Vpc, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         self.cluster = eks.Cluster(
-            self, "EksCluster",
+            self, "AestEks",
             vpc=vpc,
             version=eks.KubernetesVersion.V1_31,
-            default_capacity=2  # Default node group with 2 instances
+            default_capacity=2 
         )
+
+        self.cluster.add_manifest(
+            "DastDeployment",
+            self.load_eks_manifest(self.dast_deployment_path)
+        )
+
+        self.cluster.add_manifest(
+            "DastService",
+            self.load_eks_manifest(self.dast_service_path)
+        )
+
+    def load_eks_manifest(self, path):
+        with open(path, 'r') as file:
+            return yaml.safe_load(file)
