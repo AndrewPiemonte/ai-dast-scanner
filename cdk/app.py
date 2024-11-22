e4479f2da793229d3ae75a850e1d611480776025
Junsu An <90178502+junsu-a@users.noreply.github.com>
2024-11-21 21:06:32 -0800

    Implement backend feature to trigger ZAP scan with target url (#8)
    * Add ZAP scan job Helm chart and update backend configurations
    
    - Introduced a new Helm chart for ZAP Baseline scan job with necessary templates and values.
    - Updated backend service and deployment configurations to streamline naming conventions.
    - Enhanced Dockerfile to include installation of Helm and kubectl.
    - Added a script for building and pushing Docker images to AWS ECR.
    - Updated requirements.txt to include new dependencies for the ZAP scan job.
    
    * - Mount S3 on EKS
    - ZAP basescan to save report on S3
    - Backend read report from S3 and return the json

--- cdk/app.py
+++ cdk/app.py
@@ -3,12 +3,21 @@ import aws_cdk as cdk
 from lib.vpc_stack import AestVpcStack
 from lib.eks_stack import EksStack
 from lib.s3_stack import S3Stack
+from aws_cdk import App, CfnOutput
 
-app = cdk.App()
+app = App()
 
 vpc_stack = AestVpcStack(app, "AestVpcStack")
+s3_stack = S3Stack(app, "S3Stack", vpc=vpc_stack.vpc)
+eks_stack = EksStack(app, "EksStack", 
+    vpc=vpc_stack.vpc,
+    bucket=s3_stack.bucket
+)
 
-EksStack(app, "EksStack", vpc=vpc_stack.vpc)
-S3Stack(app, "S3Stack")
+# Output the bucket name for use in Helm values
+CfnOutput(s3_stack, "BucketName",
+    value=s3_stack.bucket.bucket_name,
+    description="Name of the S3 bucket for ZAP reports"
+)
 
 app.synth()
