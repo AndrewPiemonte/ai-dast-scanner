ea292da3ea78943a453ee9b9807b6171c76743e8
Alfredo-del-Rayo <99233341+Alfredo-del-Rayo@users.noreply.github.com>
2025-03-08 20:06:05 -0800

    removed unecessary files

--- src/frontend/amplify/functions/get-report/handler.ts
+++
@@ -1,6 +0,0 @@
-import type { Schema } from '../../data/resource'
-
-export const handler: Schema["getReport"]["functionHandler"] = async (event) => {
-    const { target_url } = event.arguments
-    return `Hello, you are trying to get a report for ${target_url}`
-}
--- src/frontend/amplify/functions/get-report/resource.ts
+++
@@ -1,6 +0,0 @@
-import { defineFunction } from '@aws-amplify/backend'
-
-export const getReport = defineFunction({
-    name: 'get-zap-report',
-    entry: './handler.ts'
-})
