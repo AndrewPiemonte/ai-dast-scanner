2b9b9bfb5cb09cbe66a51b7a5d9281f27f0a6d77
Alfredo-del-Rayo <99233341+Alfredo-del-Rayo@users.noreply.github.com>
2025-04-25 12:13:41 -0700

    added ALLOWED Domains to enviroment variables

--- src/frontend/amplify/function/preSignUpHandler.ts
+++ src/frontend/amplify/function/preSignUpHandler.ts
@@ -1,14 +1,15 @@
 import { PreSignUpTriggerEvent, Context } from 'aws-lambda';
+import { env } from '$amplify/env/preSignUpValidator';
 
 export const handler = async (event: PreSignUpTriggerEvent, _context: Context) => {
     const email = event.request.userAttributes.email;
     // Add/Change allowed email domains 
-    const allowedDomains = ['mail.ubc.ca', 'ubc.ca'];
+    const allowedDomains = env.ALLOWED_DOMAINS.split(',').map((domain: string) => domain.trim().toLowerCase());
 
     const domain = email?.split('@')[1]?.toLowerCase();
 
     if (!domain || !allowedDomains.includes(domain)) {
-        const error = new Error('Only UBC email addresses are allowed to sign up');
+        const error = new Error('Error validating email domain during pre-signup');
         throw error;
     }
 
--- src/frontend/amplify/function/preSignUpValidator.ts
+++ src/frontend/amplify/function/preSignUpValidator.ts
@@ -1,6 +1,9 @@
 import { defineFunction } from '@aws-amplify/backend';
 
 export const preSignUpValidator = defineFunction({
+  environment:{
+    ALLOWED_DOMAINS: process.env.ALLOWED_DOMAINS || '',
+  },
   name: 'preSignUpValidator',
   entry: './preSignUpHandler.ts'
 });
