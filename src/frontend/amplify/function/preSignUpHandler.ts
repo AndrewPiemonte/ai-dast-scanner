878295c77f6d2792f4f3874ec166122c0d6b7a32
Alfredo-del-Rayo <99233341+Alfredo-del-Rayo@users.noreply.github.com>
2025-04-24 17:38:38 -0700

    fixed error handling

--- src/frontend/amplify/function/preSignUpHandler.ts
+++ src/frontend/amplify/function/preSignUpHandler.ts
@@ -1,20 +1,16 @@
 import { PreSignUpTriggerEvent, Context } from 'aws-lambda';
 
 export const handler = async (event: PreSignUpTriggerEvent, _context: Context) => {
-    try {
-        const email = event.request.userAttributes.email;
-        // Add/Change allowed email domains 
-        const allowedDomains = ['mail.ubc.ca'];
+    const email = event.request.userAttributes.email;
+    // Add/Change allowed email domains 
+    const allowedDomains = ['mail.ubc.ca'];
 
-        const domain = email?.split('@')[1]?.toLowerCase();
+    const domain = email?.split('@')[1]?.toLowerCase();
 
-        if (!domain || !allowedDomains.includes(domain)) {
-            const error = new Error('Only UBC email addresses are allowed to sign up');
-            throw error;
-        }
-
-        return event;
-    } catch (error) {
-        throw new Error('Error validating email domain during pre-signup.');
+    if (!domain || !allowedDomains.includes(domain)) {
+        const error = new Error('Only UBC email addresses are allowed to sign up');
+        throw error;
     }
+
+    return event;
 };
