85a09faf75ac3fe1d5e3943707c9ac538e3aaa84
Alfredo-del-Rayo <99233341+Alfredo-del-Rayo@users.noreply.github.com>
2025-04-24 18:11:57 -0700

    added ubc.ca domain to presignup handler

--- src/frontend/amplify/function/preSignUpHandler.ts
+++ src/frontend/amplify/function/preSignUpHandler.ts
@@ -3,7 +3,7 @@ import { PreSignUpTriggerEvent, Context } from 'aws-lambda';
 export const handler = async (event: PreSignUpTriggerEvent, _context: Context) => {
     const email = event.request.userAttributes.email;
     // Add/Change allowed email domains 
-    const allowedDomains = ['mail.ubc.ca'];
+    const allowedDomains = ['mail.ubc.ca', 'ubc.ca'];
 
     const domain = email?.split('@')[1]?.toLowerCase();
 
